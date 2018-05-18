d2dcaac6a1c2f75244cb19564c2257a0a9d52f96
mayfa <pavel.akira.marek@gmail.com>
2018-05-18 16:03:55 +0200

    TcpDriver: add skeleton for PC

--- communication/tcp_driver.cpp
+++ communication/tcp_driver.cpp
@@ -1,5 +1,43 @@
-//
-// Created by mayfa on 14.5.18.
-//
+/**
+ * @file tcp_driver.cpp
+ * @brief TCP driver implemented with BSD sockets.
+ */
 
 #include "tcp_driver.hpp"
+#include "rt_assert.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <cstdio>
+
+namespace comm {
+
+void TcpDriver::connect(const char *ip_addr, const uint16_t port)
+{
+    struct addrinfo hints;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    char port_str[7];
+    std::sprintf(port_str, "%d", port);
+    struct addrinfo *res;
+    getaddrinfo(ip_addr, port_str, &hints, &res);
+
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(socketFd, res->ai_addr, res->ai_addrlen) != 0) {
+        std::fprintf(stderr, "connect");
+    }
+
+    connected = true;
+}
+
+bool TcpDriver::send(const uint8_t *buff, const size_t buff_len)
+{
+    rt_assert(connected, "TcpDriver: not connected");
+    ssize_t sent = ::send(socketFd, buff, buff_len, 0);
+    return sent == buff_len;
+}
+
+} // namespace comm
--- communication/tcp_driver.hpp
+++ communication/tcp_driver.hpp
@@ -5,4 +5,24 @@
 #ifndef DEVICE_SIMULATOR_TCP_DRIVER_HPP_HPP
 #define DEVICE_SIMULATOR_TCP_DRIVER_HPP_HPP
 
+#include <cstdint>
+#include <cstddef>
+
+namespace comm {
+
+/**
+ * Common driver declaration for both BSD socket and LwIP implementation.
+ */
+class TcpDriver {
+public:
+    static void connect(const char *ip_addr, const uint16_t port);
+    static bool send(const uint8_t *buff, const size_t buff_len);
+
+private:
+    static int socketFd;
+    static bool connected;
+};
+
+} // namespace comm
+
 #endif //DEVICE_SIMULATOR_TCP_DRIVER_HPP_HPP
