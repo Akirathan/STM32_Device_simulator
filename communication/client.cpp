ff0a7e6277bdc028aaab40ccaf96d914a1c2bf63
mayfa <pavel.akira.marek@gmail.com>
2018-05-18 16:03:00 +0200

    Client: prepare for callback API

--- communication/client.cpp
+++ communication/client.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "client.hpp"
+#include "http/response_parser.hpp"
+
+namespace comm {
+
+
+void Client::receiveCb(const uint8_t *buff, const size_t buff_size)
+{
+    switch (state) {
+        case AWAIT_CONNECT_RESPONSE:
+            http::Response response;
+            if (!readResponse(buff, buff_size, &response)) {
+
+            }
+            break;
+        case AWAIT_INTERVAL_TIMESTAMP_RESPONSE:break;
+        case AWAIT_INTERVALS:break;
+        case AWAIT_INTERVALS_ACK:break;
+        case AWAIT_TEMP_ACK:break;
+    }
+}
+
+/**
+ *
+ * @param response [out]
+ * @return bool whether parsing succeed
+ */
+bool Client::readResponse(const uint8_t *buff, const size_t buff_size, http::Response *response)
+{
+    return http::ResponseParser::parse(static_cast<const char *>(buff), buff_size, response);
+}
+
+} // namespace comm
--- communication/client.hpp
+++ communication/client.hpp
@@ -6,19 +6,34 @@
 #define DEVICE_SIMULATOR_CLIENT_HPP
 
 #include <cstdint>
+#include "http/response.hpp"
+#include "device.hpp"
+#include "interval_list.hpp"
 
 namespace comm {
 
 /**
 * Represents application specific client.
-*
-* @note communication may always result in error that'swhy all function members that use
-*       TCP/IP returns bool (or other representation of error).
+ *
+ * Conforms to callback API.
 */
 class Client {
+    enum await_state_t {
+        AWAIT_CONNECT_RESPONSE,
+        AWAIT_INTERVAL_TIMESTAMP_RESPONSE,
+        AWAIT_INTERVALS,
+        AWAIT_INTERVALS_ACK,
+        AWAIT_TEMP_ACK
+    };
+
 public:
     static void init(const char *ip_addr, uint16_t port);
-    static bool connect(Device &device, uint32_t *server_real_time);
+    /**
+     * This method is specific for BSD socket API.
+     */
+    static void poll();
+    static void receiveCb(const uint8_t *buff, const size_t buff_size);
+    static bool sendConnectReq(Device *device, uint32_t *server_real_time);
     static bool sendTemperature(const double temp, const uint32_t time_stamp);
     static bool sendIntervals(const IntervalList &intervals, const uint32_t time_stamp);
     static bool receiveIntervals(IntervalList *intervals);
@@ -28,6 +43,10 @@ private:
     static bool initialized;
     static char ipAddr[9];
     static uint16_t port;
+    static await_state_t state;
+    static Device *currDevice;
+
+    static bool readResponse(const uint8_t *buff, const size_t buff_size, http::Response *response);
 };
 
 } // namespace comm
