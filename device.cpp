3fba38e1324bcd94b1c6b6d4f9e5f66ce33cdd09
mayfa <pavel.akira.marek@gmail.com>
2018-05-19 13:25:47 +0200

    Device: add skeleton and setConnected

--- device.cpp
+++ device.cpp
@@ -2,4 +2,38 @@
 // Created by mayfa on 14.5.18.
 //
 
+#include <cstring>
 #include "device.hpp"
+#include "communication/client.hpp"
+
+Device::Device(const char *id, const char *key) :
+        temp(0),
+        connected(false)
+{
+    std::strcpy(this->id, id);
+    std::strcpy(this->key, key);
+}
+
+const char *Device::getKey() const
+{
+    return key;
+}
+
+/**
+ * Sets connected state of the device ie. it is connected to the server.
+ */
+void Device::setConnected()
+{
+    connected = true;
+}
+
+/**
+ * Sends connect request to server.
+ * @return
+ */
+bool Device::connect()
+{
+    bool ret_val = false;
+    ret_val = comm::Client::sendConnectReq(this);
+}
+
--- device.hpp
+++ device.hpp
@@ -9,19 +9,26 @@
 #include "communication/interval_list.hpp"
 
 class Device {
+private:
+    static const size_t KEY_LEN = 9;
+    static const size_t ID_LEN = 15;
+
 public:
-    Device(const std::string &id, const std::string &key);
-    bool connect();
+    Device(const char *id, const char *key);
+    const char * getKey() const;
+    uint32_t getTemp() const;
     void setTemp(uint32_t temp);
     void setIntervals(const comm::IntervalList &interval_list);
-    uint32_t getTemp() const;
     const comm::IntervalList & getIntervals() const;
+    void setConnected();
+    bool connect();
 
 private:
-    std::string key;
-    std::string id;
+    char key[KEY_LEN];
+    char id[ID_LEN];
     uint32_t temp;
     comm::IntervalList intervalList;
+    bool connected;
 };
 
 
